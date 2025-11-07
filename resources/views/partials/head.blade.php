f25eb44503ccc4ded9b824a7064a86a210699b96
LuisBravoZ <144957356+LuisBravoZ@users.noreply.github.com>
2025-11-07 18:44:02 -0500

    se hizo  la pagina principal del sitio web

--- resources/views/partials/head.blade.php
+++ resources/views/partials/head.blade.php
@@ -7,6 +7,10 @@
 <link rel="icon" href="/favicon.svg" type="image/svg+xml">
 <link rel="apple-touch-icon" href="/apple-touch-icon.png">
 
+    <!-- Google Material Symbols -->
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined" />
+
+   
 <link rel="preconnect" href="https://fonts.bunny.net">
 <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
 
