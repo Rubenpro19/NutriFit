6f91b22810c2b205b8c12fad3aba95509f91bd7f
Rubenpro19 <dariomera911@gmail.com>
2025-11-07 18:48:46 -0500

    Se agregó el header y footer en el login y registro

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -1,18 +1,18 @@
-<!DOCTYPE html>
-<html lang="es">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Administrador</title>
-</head>
-<body>
-    <h1>Este es el panel de administración</h1>
-    <form method="POST" action="{{ route('logout') }}" class="w-full">
-        @csrf
-        <flux:menu.item as="button" type="submit" icon="arrow-right-start-on-rectangle" class="w-full">
-            {{ __('Cerrar sesión') }}
-        </flux:menu.item>
-    </form>
-</body>
-</html>
+<x-layouts.app :title="__('Panel de Administración')">
+    <div class="flex h-full w-full flex-1 flex-col gap-4 rounded-xl">
+        <div class="grid auto-rows-min gap-4 md:grid-cols-3">
+            <div class="relative aspect-video overflow-hidden rounded-xl border border-neutral-200 dark:border-neutral-700">
+                <x-placeholder-pattern class="absolute inset-0 size-full stroke-gray-900/20 dark:stroke-neutral-100/20" />
+            </div>
+            <div class="relative aspect-video overflow-hidden rounded-xl border border-neutral-200 dark:border-neutral-700">
+                <x-placeholder-pattern class="absolute inset-0 size-full stroke-gray-900/20 dark:stroke-neutral-100/20" />
+            </div>
+            <div class="relative aspect-video overflow-hidden rounded-xl border border-neutral-200 dark:border-neutral-700">
+                <x-placeholder-pattern class="absolute inset-0 size-full stroke-gray-900/20 dark:stroke-neutral-100/20" />
+            </div>
+        </div>
+        <div class="relative h-full flex-1 overflow-hidden rounded-xl border border-neutral-200 dark:border-neutral-700">
+            <x-placeholder-pattern class="absolute inset-0 size-full stroke-gray-900/20 dark:stroke-neutral-100/20" />
+        </div>
+    </div>
+</x-layouts.app>
--- resources/views/paciente/dashboard.blade.php
+++ resources/views/paciente/dashboard.blade.php
@@ -1,18 +1,27 @@
 <!DOCTYPE html>
 <html lang="es">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Panel Paciente</title>
+    
 </head>
+
 <body>
     <h1>Bienvenido al Panel Paciente</h1>
-    <form method="POST" action="{{ route('logout') }}" class="w-full">
+    <form method="POST" action="{{ route('logout') }}">
         @csrf
-        <flux:menu.item as="button" type="submit" icon="arrow-right-start-on-rectangle" class="w-full">
+        <flux:menu.item as="button" type="submit" icon="arrow-right-start-on-rectangle">
             {{ __('Cerrar sesión') }}
         </flux:menu.item>
     </form>
+
+    <a href="{{ url('/dashboard') }}"
+        class="inline-block px-5 py-1.5 dark:text-[#EDEDEC] border-[#19140035] hover:border-[#1915014a] border text-[#1b1b18] dark:border-[#3E3E3A] dark:hover:border-[#62605b] rounded-sm text-sm leading-normal">
+        Dashboard
+    </a>
 </body>
+
 </html>
